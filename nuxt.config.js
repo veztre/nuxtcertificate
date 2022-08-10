94ed0666e58144a9deede52f1faef83de7f12927
Enrico P. Chavez <vez3@yahoo.com>
2022-08-10 18:47:14 +0800

    add target static to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  
   head: {
     titleTemplate: '%s - nuxtcertificate',
     title: 'nuxtcertificate',
@@ -61,5 +63,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+
   }
+
+
 }
